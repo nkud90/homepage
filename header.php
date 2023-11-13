dd361d075a88e8da6b900b07949a96f30e16767d
Nikolai Kudriavtsev <nkud90@gmail.com>
2023-11-13 14:53:49 +0300

    changed nkud.ru to nkud.me in header

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <header>
-    <a href="/index.php" class="logo">nkud.ru</a>
+    <a href="/index.php" class="logo">nkud.me</a>
     <!-- <a href="#" class="menu" onclick="darkMode()"><img src="/assets/icons/darkmode.svg" style="vertical-align:middle" alt="Dark mode icon" title="Switch dark and light mode" width="24" height="24" class="icon"></a>
     <a href="#" class="menu"><img src="/assets/icons/lightning.svg" style="vertical-align:middle" alt="Tip with Bitcoin Lightning" title="Tip with Bitcoin Lightning" width="24" height="24" class="icon"></a>
     <a href="#" class="menu">contact</a>
