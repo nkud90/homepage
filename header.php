3f2b0d408abb72c306a212940364462677a2f30d
Nikolai Kudriavtsev <nkud90@gmail.com>
2023-11-29 21:25:47 +0300

    fixed the contact link

--- header.php
+++ header.php
@@ -3,5 +3,5 @@
     <!-- <a href="#" class="menu" onclick="darkMode()"><img src="/assets/icons/darkmode.svg" style="vertical-align:middle" alt="Dark mode icon" title="Switch dark and light mode" width="24" height="24" class="icon"></a>
     <a href="#" class="menu"><img src="/assets/icons/lightning.svg" style="vertical-align:middle" alt="Tip with Bitcoin Lightning" title="Tip with Bitcoin Lightning" width="24" height="24" class="icon"></a>
     <a href="#" class="menu">about</a> -->
-    <a href="contact.php" class="menu">✉️ Contact</a>
+    <a href="/contact.php" class="menu">✉️ Contact</a>
 </header>
